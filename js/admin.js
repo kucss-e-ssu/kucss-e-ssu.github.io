f7e0ff4cb8d82b808e99bcac4ec729110ef7cda8
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2023-10-05 02:26:26 +0900

    GGrobee : develop admin.js in localStorage

---
+++ js/admin.js
@@ -0,0 +1,6 @@
+// 방명록에서 이상한 악성 글 삭제할 때 사용하는 코드
+
+guestbook = localStorage.guestbookEntries;
+value = JSON.parse(guestbook);
+value.splice(삭제할 인덱스, 1);
+localStorage.setItem('guestbookEntries', JSON.stringify(value));
