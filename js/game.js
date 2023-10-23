752b366c4628ea4516094a0242085830443d96cc
ChanYoungJo <144414355+ChanYoungJo@users.noreply.github.com>
2023-10-23 22:53:10 +0700

    GGrobee : add contents to all page

--- js/game.js
+++ js/game.js
@@ -1,39 +1,3 @@
-GGrobee = document.querySelector('#GGrobee');
-About = document.querySelector('#About');
-What = document.querySelector('#What');
-SDGS = document.querySelector('#SDGS');
-Com = document.querySelector('#Com');
-anc = document.querySelector('#anc');
-
-iframe = document.querySelector('#iframe');
-
-GGrobee.addEventListener('click', function() {
-  // iframe.setAttribute('src', 'asset/ggrobee.html');
-  iframe.src = 'asset/ggrobee.html';
-});
-
-About.addEventListener('click', function() {
-    iframe.src = 'asset/about.html';
-});
-
-What.addEventListener('click', function() {
-    iframe.src = 'asset/pollution.html';
-});
-
-SDGS.addEventListener('click', function() {
-  iframe.src = 'asset/sdgs.html';
-});
-
-Com.addEventListener('click', function() {
-  iframe.src = 'asset/com.html';
-});
-
-anc.addEventListener('click', function() {
-  iframe.src = 'asset/anc.html';
-});
-
-
-
 const player = document.querySelector('.player');
 const poop = document.querySelector('.poop');
 let isGameOver = false;
